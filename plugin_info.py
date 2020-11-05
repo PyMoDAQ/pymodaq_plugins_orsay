696013b94a68f646e6d8b7891996233423674bd9
Sébastien Weber <seba.weber@gmail.com>
2020-11-05 21:54:08 +0100

    update plugin info

--- plugin_info.py
+++ plugin_info.py
@@ -1,9 +1,12 @@
 ## To modify by developper(s) of the plugin
 SHORT_PLUGIN_NAME = 'orsay'
 package_url = 'https://github.com/CEMES-CNRS/pymodaq_plugins_orsay'
+description = f'PyMoDAQ Orsay STEM and Camera plugin to control Ropers or Princeton Cameras and STEM (Scanning Tunnel' \
+              f' Electron Microscope) using the HOUDs2 electronic module '
 
-author = 'Sébastien Weber',
-author_email = 'sebastien.weber@cemes.fr',
+
+author = 'Sébastien Weber'
+author_email = 'sebastien.weber@cemes.fr'
 
 #packages required for your plugin:
 packages_required = []
